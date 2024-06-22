cbf5f9b6946a04cb0337b3c1c4a19e0496d117f9
minzinphyo <minzinphyo143@gmail.com>
2024-06-22 21:04:34 +0900

    Initial Commit

---
+++ Swift-UI-BootCamp/Swift_UI_BootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_BootCampApp.swift
+//  Swift-UI-BootCamp
+//
+//  Created by Min Zin Phyo on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_BootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
